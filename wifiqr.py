9ed026ba001e8e8dc54afdd3713d38cc6123f7fa
Doğukan KURNAZ <dogukankurnaz@gmail.com>
2023-01-04 01:06:35 +0300

    Update wifiqr.py

--- wifiqr.py
+++ wifiqr.py
@@ -1,7 +1,7 @@
 from pickle import FALSE
 import wifi_qrcode_generator as qr
 
-qrCode = qr.wifi_qrcode('WIFI NAME',False,'WPA','PASSWORD') #EDIT THIS BLOCK
+qrCode = qr.wifi_qrcode('WIFI NAME',False,'WPA','PASSWORD') #EDIT THIS BLOCK (WIFINAME AND WIFI PASSWORD)
 qrCode.show()
 
 qrCode.save('qrcode.png')
